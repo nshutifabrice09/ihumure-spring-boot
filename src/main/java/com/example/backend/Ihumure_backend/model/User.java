e089f832ef19a20d28ef1207bb2b667102c62336
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-16 23:30:05 +0200

    model commit

--- src/main/java/com/example/backend/Ihumure_backend/model/User.java
+++ src/main/java/com/example/backend/Ihumure_backend/model/User.java
@@ -1,2 +1,20 @@
-package com.example.backend.Ihumure_backend.model;public class User {
+package com.example.backend.Ihumure_backend.model;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToOne;
+
+public class User {
+    private Long id;
+
+    private String email;
+    private String password;
+    private String fullName;
+    private boolean active;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToOne(mappedBy = "user")
+    private UserProfile profile;
 }
--- src/main/java/com/example/backend/Ihumure_backend/model/UserProfile.java
+++ src/main/java/com/example/backend/Ihumure_backend/model/UserProfile.java
@@ -1,2 +1,19 @@
-package com.example.backend.Ihumure_backend.model;public class UserProfile {
+package com.example.backend.Ihumure_backend.model;
+
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+import java.time.LocalDate;
+
+public class UserProfile {
+    private Long id;
+
+    private String bio;
+    private String language;
+    private LocalDate birthDate;
+    private String gender;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
