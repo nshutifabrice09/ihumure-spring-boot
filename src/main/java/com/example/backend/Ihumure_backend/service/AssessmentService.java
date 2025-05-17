541cb537c80ffb0bcf4fff99d1e355081a1a2bf9
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-17 21:42:44 +0200

    service-commit

--- src/main/java/com/example/backend/Ihumure_backend/service/AssessmentService.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/AssessmentService.java
@@ -1,2 +1,13 @@
-package com.example.backend.Ihumure_backend.service;public interface AssessmentService {
+package com.example.backend.Ihumure_backend.service;
+
+import com.example.backend.Ihumure_backend.model.Assessment;
+
+import java.util.List;
+
+public interface AssessmentService {
+    List<Assessment> getAllAssessments();
+    Assessment getAssessmentById(Long id);
+    Assessment save(Assessment assessment, Long userId);
+    Assessment updateAssessment(Long id, Assessment assessment);
+    void removeById(Long id);
 }
