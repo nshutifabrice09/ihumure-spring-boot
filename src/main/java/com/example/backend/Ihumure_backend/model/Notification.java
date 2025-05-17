3e10a680f46d88544253405d39ff498c01d92263
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-17 10:03:27 +0200

    initial-model-updates

--- src/main/java/com/example/backend/Ihumure_backend/model/Notification.java
+++ src/main/java/com/example/backend/Ihumure_backend/model/Notification.java
@@ -1,2 +1,16 @@
-package com.example.backend.Ihumure_backend.model;public class Notification {
+package com.example.backend.Ihumure_backend.model;
+
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDate;
+
+public class Notification {
+    private Long id;
+
+    @ManyToOne
+    private User recipient;
+
+    private String message;
+    private boolean read;
+    private LocalDate sentAt;
 }
