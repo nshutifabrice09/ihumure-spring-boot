bc7cde67e1791d2c8a89140788a4b443f11c651f
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-17 10:18:03 +0200

    final-model-annotation-commit

--- src/main/java/com/example/backend/Ihumure_backend/model/GroupPost.java
+++ src/main/java/com/example/backend/Ihumure_backend/model/GroupPost.java
@@ -1,10 +1,22 @@
 package com.example.backend.Ihumure_backend.model;
 
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+@Data
+@Builder
+@Entity
+@Table(name = "group_posts")
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class GroupPost {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
--- src/main/java/com/example/backend/Ihumure_backend/model/Notification.java
+++ src/main/java/com/example/backend/Ihumure_backend/model/Notification.java
@@ -1,10 +1,21 @@
 package com.example.backend.Ihumure_backend.model;
 
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-
+@Data
+@Builder
+@Entity
+@Table(name = "notifications")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
