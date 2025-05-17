1c6a977042349024cf2a8aa4ff96ba9973e46f7c
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-17 10:51:29 +0200

    repo-commit

--- src/main/java/com/example/backend/Ihumure_backend/repository/AssessmentAnswerRepository.java
+++ src/main/java/com/example/backend/Ihumure_backend/repository/AssessmentAnswerRepository.java
@@ -1,2 +1,9 @@
-package com.example.backend.Ihumure_backend.repository;public interface AssessmentAnswerRepository {
+package com.example.backend.Ihumure_backend.repository;
+
+import com.example.backend.Ihumure_backend.model.AssessmentAnswer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AssessmentAnswerRepository extends JpaRepository<AssessmentAnswer, Long> {
 }
--- src/main/java/com/example/backend/Ihumure_backend/repository/AssessmentQuestionRepository.java
+++ src/main/java/com/example/backend/Ihumure_backend/repository/AssessmentQuestionRepository.java
@@ -1,2 +1,9 @@
-package com.example.backend.Ihumure_backend.repository;public interface AssessmentQuestionRepository {
+package com.example.backend.Ihumure_backend.repository;
+
+import com.example.backend.Ihumure_backend.model.AssessmentQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AssessmentQuestionRepository extends JpaRepository<AssessmentQuestion, Long> {
 }
