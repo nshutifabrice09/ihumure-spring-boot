73e3f978240fd6729e308da80b861e4653200b50
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-17 10:54:58 +0200

    repo-commit

--- src/main/java/com/example/backend/Ihumure_backend/repository/SupportGroupRepository.java
+++ src/main/java/com/example/backend/Ihumure_backend/repository/SupportGroupRepository.java
@@ -1,2 +1,9 @@
-package com.example.backend.Ihumure_backend.repository;public interface SupportGroupRepository {
+package com.example.backend.Ihumure_backend.repository;
+
+import com.example.backend.Ihumure_backend.model.SupportGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupportGroupRepository extends JpaRepository<SupportGroup, Long> {
 }
--- src/main/java/com/example/backend/Ihumure_backend/repository/UserRepository.java
+++ src/main/java/com/example/backend/Ihumure_backend/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.example.backend.Ihumure_backend.repository;public interface UserRepository {
+package com.example.backend.Ihumure_backend.repository;
+
+import com.example.backend.Ihumure_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long> {
 }
