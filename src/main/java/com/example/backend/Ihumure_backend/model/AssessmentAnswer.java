b323c79c031093063a42435a6499e6548cef1c91
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-17 10:08:26 +0200

    model-updates

--- src/main/java/com/example/backend/Ihumure_backend/model/AssessmentAnswer.java
+++ src/main/java/com/example/backend/Ihumure_backend/model/AssessmentAnswer.java
@@ -1,8 +1,20 @@
 package com.example.backend.Ihumure_backend.model;
 
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@Entity
+@Table(name = "assessment_answers")
+@NoArgsConstructor
+@AllArgsConstructor
 public class AssessmentAnswer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
--- src/main/java/com/example/backend/Ihumure_backend/model/AssessmentQuestion.java
+++ src/main/java/com/example/backend/Ihumure_backend/model/AssessmentQuestion.java
@@ -1,6 +1,20 @@
 package com.example.backend.Ihumure_backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@Table(name = "assessment_questions")
+@NoArgsConstructor
+@AllArgsConstructor
 public class AssessmentQuestion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String questionText;
