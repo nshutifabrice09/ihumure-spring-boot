14c709d3b32d7906f13574192cb97b7bc9f9d48f
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-17 10:50:19 +0200

    repo-commit

--- src/main/java/com/example/backend/Ihumure_backend/repository/AppointmentRepository.java
+++ src/main/java/com/example/backend/Ihumure_backend/repository/AppointmentRepository.java
@@ -1,2 +1,9 @@
-package com.example.backend.Ihumure_backend.repository;public interface AppointmentRepository {
+package com.example.backend.Ihumure_backend.repository;
+
+import com.example.backend.Ihumure_backend.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository <Appointment, Long>{
 }
--- src/main/java/com/example/backend/Ihumure_backend/repository/AssessmentRepository.java
+++ src/main/java/com/example/backend/Ihumure_backend/repository/AssessmentRepository.java
@@ -1,2 +1,9 @@
-package com.example.backend.Ihumure_backend.repository;public class AssessmentRepository {
+package com.example.backend.Ihumure_backend.repository;
+
+import com.example.backend.Ihumure_backend.model.Assessment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AssessmentRepository extends JpaRepository <Assessment, Long> {
 }
