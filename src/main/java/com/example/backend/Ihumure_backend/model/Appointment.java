8d6f9e70b80e32eda8838734e74f81f68867a2f7
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-17 10:06:56 +0200

    model-updates

--- src/main/java/com/example/backend/Ihumure_backend/model/Appointment.java
+++ src/main/java/com/example/backend/Ihumure_backend/model/Appointment.java
@@ -1,10 +1,22 @@
 package com.example.backend.Ihumure_backend.model;
 
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Data
+@Builder
+@Entity
+@Table(name = "appointments")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
--- src/main/java/com/example/backend/Ihumure_backend/model/Assessment.java
+++ src/main/java/com/example/backend/Ihumure_backend/model/Assessment.java
@@ -1,12 +1,22 @@
 package com.example.backend.Ihumure_backend.model;
 
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
-
+@Data
+@Builder
+@Entity
+@Table(name = "assessments")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Assessment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
