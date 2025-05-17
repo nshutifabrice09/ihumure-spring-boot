0e79a311a697e8282faa4673b8fa20693da3e3f0
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-17 10:52:32 +0200

    repo-commit

--- src/main/java/com/example/backend/Ihumure_backend/repository/CommentRepository.java
+++ src/main/java/com/example/backend/Ihumure_backend/repository/CommentRepository.java
@@ -1,2 +1,9 @@
-package com.example.backend.Ihumure_backend.repository;public interface CommentRepository {
+package com.example.backend.Ihumure_backend.repository;
+
+import com.example.backend.Ihumure_backend.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
--- src/main/java/com/example/backend/Ihumure_backend/repository/GroupPostRepository.java
+++ src/main/java/com/example/backend/Ihumure_backend/repository/GroupPostRepository.java
@@ -1,2 +1,9 @@
-package com.example.backend.Ihumure_backend.repository;public interface GroupPostRepository {
+package com.example.backend.Ihumure_backend.repository;
+
+import com.example.backend.Ihumure_backend.model.GroupPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupPostRepository extends JpaRepository<GroupPost, Long> {
 }
