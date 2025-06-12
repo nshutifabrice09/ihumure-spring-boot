d8637dc680e716644857804e0166ee5c84b0498e
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-12 10:08:55 +0200

    service-impl-updates

--- src/main/java/com/example/backend/Ihumure_backend/service/CommentService.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/CommentService.java
@@ -1,2 +1,13 @@
-package com.example.backend.Ihumure_backend.service;public interface CommetService {
+package com.example.backend.Ihumure_backend.service;
+
+import com.example.backend.Ihumure_backend.model.Comment;
+
+import java.util.List;
+
+public interface CommentService {
+    List<Comment> getAllComments();
+    Comment getCommentById(Long id);
+    Comment save(Comment comment, Long postId, Long authorId);
+    Comment updateComment(Long id, Comment comment);
+    void removeId(Long id);
 }
