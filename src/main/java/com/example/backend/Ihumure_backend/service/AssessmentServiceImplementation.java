9d443ebc23ed8ccb489c82099e9875bb631f54bd
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-12 10:29:35 +0200

    repo-update

--- src/main/java/com/example/backend/Ihumure_backend/service/AssessmentServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/AssessmentServiceImplementation.java
@@ -1,8 +1,34 @@
 package com.example.backend.Ihumure_backend.service;
 
-import com.example.backend.Ihumure_backend.repository.AssessmentRepository;
+import com.example.backend.Ihumure_backend.model.Assessment;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class AssessmentServiceImplementation implements AssessmentRepository {
+public class AssessmentServiceImplementation implements AssessmentService{
+    @Override
+    public List<Assessment> getAllAssessments() {
+        return null;
+    }
+
+    @Override
+    public Assessment getAssessmentById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Assessment save(Assessment assessment, Long userId) {
+        return null;
+    }
+
+    @Override
+    public Assessment updateAssessment(Long id, Assessment assessment) {
+        return null;
+    }
+
+    @Override
+    public void removeById(Long id) {
+
+    }
 }
