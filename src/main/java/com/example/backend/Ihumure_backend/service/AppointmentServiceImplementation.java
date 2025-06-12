3311e9f4fac757001d95ae41bb03bae20de04d2f
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-12 10:10:24 +0200

    service-impl-parameters

--- src/main/java/com/example/backend/Ihumure_backend/service/AppointmentServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/AppointmentServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.example.backend.Ihumure_backend.service;public class AppointmentServiceImplementation {
+package com.example.backend.Ihumure_backend.service;
+
+import com.example.backend.Ihumure_backend.model.Appointment;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AppointmentServiceImplementation implements AppointmentService{
+    @Override
+    public List<Appointment> getAllAppointments() {
+        return null;
+    }
+
+    @Override
+    public Appointment getAppointmentById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Appointment save(Appointment appointment, Long therapyId, Long userId) {
+        return null;
+    }
+
+    @Override
+    public Appointment updateAppointment(Long id, Appointment appointment) {
+        return null;
+    }
+
+    @Override
+    public void removeById(Long id) {
+
+    }
 }
