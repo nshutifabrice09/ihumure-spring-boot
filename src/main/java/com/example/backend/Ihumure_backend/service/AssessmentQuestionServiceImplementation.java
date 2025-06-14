5c9c9ecccb271b7f5f0473708bb08ee0c273ede1
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-15 00:40:23 +0200

    implement-update-commit

--- src/main/java/com/example/backend/Ihumure_backend/service/AssessmentQuestionServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/AssessmentQuestionServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.example.backend.Ihumure_backend.service;
 
+import com.example.backend.Ihumure_backend.model.AssessmentQuestion;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class AssessmentQuestionServieImplementation implements AssessmentQuestionService{
+public class AssessmentQuestionServiceImplementation implements AssessmentQuestionService{
+    @Override
+    public List<AssessmentQuestion> getAllAssessmentQuestions() {
+        return null;
+    }
+
+    @Override
+    public AssessmentQuestion getAssessmentQuestionById(Long id) {
+        return null;
+    }
+
+    @Override
+    public AssessmentQuestion save(AssessmentQuestion assessmentQuestion) {
+        return null;
+    }
+
+    @Override
+    public AssessmentQuestion updateAssessmentQuestion(Long id, AssessmentQuestion assessmentQuestion) {
+        return null;
+    }
+
+    @Override
+    public void removeById(Long id) {
+
+    }
 }
