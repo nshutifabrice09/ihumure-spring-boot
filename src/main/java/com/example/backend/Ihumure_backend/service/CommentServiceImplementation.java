d514d44d527943014f3cec2bcfb980ef6d788777
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-15 00:49:50 +0200

    implement-constructor-commit

--- src/main/java/com/example/backend/Ihumure_backend/service/CommentServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/CommentServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.example.backend.Ihumure_backend.service;
 
+import com.example.backend.Ihumure_backend.model.Comment;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CommentServiceImplementation implements CommentService{
+    @Override
+    public List<Comment> getAllComments() {
+        return null;
+    }
+
+    @Override
+    public Comment getCommentById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Comment save(Comment comment, Long postId, Long authorId) {
+        return null;
+    }
+
+    @Override
+    public Comment updateComment(Long id, Comment comment) {
+        return null;
+    }
+
+    @Override
+    public void removeId(Long id) {
+
+    }
 }
