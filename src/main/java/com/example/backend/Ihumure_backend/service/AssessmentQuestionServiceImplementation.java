3f12e8c2af6b4e4b709928d8b0ab89821dc02075
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-15 00:45:43 +0200

    implement-update-commit

--- src/main/java/com/example/backend/Ihumure_backend/service/AssessmentQuestionServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/AssessmentQuestionServiceImplementation.java
@@ -1,12 +1,22 @@
 package com.example.backend.Ihumure_backend.service;
 
 import com.example.backend.Ihumure_backend.model.AssessmentQuestion;
+import com.example.backend.Ihumure_backend.repository.AssessmentQuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AssessmentQuestionServiceImplementation implements AssessmentQuestionService{
+    private final AssessmentQuestionRepository assessmentQuestionRepository;
+
+    @Autowired
+   public AssessmentQuestionServiceImplementation(AssessmentQuestionRepository assessmentQuestionRepository){
+       this.assessmentQuestionRepository = assessmentQuestionRepository;
+    }
+
+
     @Override
     public List<AssessmentQuestion> getAllAssessmentQuestions() {
         return null;
