97332201e3f520179b8419f5c94f7e67cbdd1f0e
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-16 09:18:55 +0200

    service-impl-commit

--- src/main/java/com/example/backend/Ihumure_backend/service/CommentServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/CommentServiceImplementation.java
@@ -2,6 +2,7 @@ package com.example.backend.Ihumure_backend.service;
 
 import com.example.backend.Ihumure_backend.model.Comment;
 import com.example.backend.Ihumure_backend.model.GroupPost;
+import com.example.backend.Ihumure_backend.model.User;
 import com.example.backend.Ihumure_backend.repository.CommentRepository;
 import com.example.backend.Ihumure_backend.repository.GroupPostRepository;
 import com.example.backend.Ihumure_backend.repository.UserRepository;
@@ -36,15 +37,25 @@ public class CommentServiceImplementation implements CommentService{
     @Override
     public Comment save(Comment comment, Long postId, Long authorId) {
         GroupPost groupPost = groupPostRepository.findGroupPostById(postId);
+        User user = userRepository.findUserById(authorId);
+        comment.setPost(groupPost);
+        comment.setAuthor(user);
+        return commentRepository.save(comment);
     }
 
     @Override
     public Comment updateComment(Long id, Comment comment) {
+        Comment existComment = commentRepository.findCommentById(id);
+        if(existComment !=null){
+            existComment.setText(comment.getText());
+            existComment.setCreatedAt(comment.getCreatedAt());
+            return commentRepository.save(existComment);
+        }
         return null;
     }
 
     @Override
     public void removeId(Long id) {
-
+        commentRepository.deleteById(id);
     }
 }
