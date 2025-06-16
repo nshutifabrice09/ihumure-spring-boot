9683d3407196d0a2c6a8fda94d1bc50f00024338
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-16 11:01:38 +0200

    service-impl-commit

--- src/main/java/com/example/backend/Ihumure_backend/service/SupportGroupServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/SupportGroupServiceImplementation.java
@@ -1,12 +1,21 @@
 package com.example.backend.Ihumure_backend.service;
 
 import com.example.backend.Ihumure_backend.model.SupportGroup;
+import com.example.backend.Ihumure_backend.repository.SupportGroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class SupportGroupServiceImplementation implements SupportGroupService{
+
+    private final SupportGroupRepository supportGroupRepository;
+
+    @Autowired
+    public SupportGroupServiceImplementation(SupportGroupRepository supportGroupRepository){
+        this.supportGroupRepository = supportGroupRepository;
+    }
     @Override
     public List<SupportGroup> getAllSupportGroups() {
         return null;
