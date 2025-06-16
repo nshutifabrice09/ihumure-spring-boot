d364bc93062b8663be16fa80b454f0438c18f5fd
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-16 21:59:38 +0200

    service-impl-commit

--- src/main/java/com/example/backend/Ihumure_backend/service/UserProfileService.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/UserProfileService.java
@@ -1,2 +1,13 @@
-package com.example.backend.Ihumure_backend.service;public interface UserProfileService {
+package com.example.backend.Ihumure_backend.service;
+
+import com.example.backend.Ihumure_backend.model.UserProfile;
+
+import java.util.List;
+
+public interface UserProfileService {
+    List<UserProfile> getAllUserProfiles();
+    UserProfile getUserProfileById(Long id);
+    UserProfile saveUserProfile(UserProfile userProfile, Long userId);
+    UserProfile updateUserProfile(Long id, UserProfile userProfile);
+    void removeUserProfile(Long id);
 }
