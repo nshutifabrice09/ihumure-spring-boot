c126e35d7e7f2c9f51116602addee8d785124a46
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-16 10:56:01 +0200

    service-impl-commit

--- src/main/java/com/example/backend/Ihumure_backend/service/SupportGroupServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/SupportGroupServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.example.backend.Ihumure_backend.service;
 
+import com.example.backend.Ihumure_backend.model.SupportGroup;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SupportGroupServiceImplementation implements SupportGroupService{
+    @Override
+    public List<SupportGroup> getAllSupportGroups() {
+        return null;
+    }
+
+    @Override
+    public SupportGroup getSupportGroupById(Long id) {
+        return null;
+    }
+
+    @Override
+    public SupportGroup save(SupportGroup supportGroup) {
+        return null;
+    }
+
+    @Override
+    public SupportGroup update(Long id, SupportGroup supportGroup) {
+        return null;
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
 }
