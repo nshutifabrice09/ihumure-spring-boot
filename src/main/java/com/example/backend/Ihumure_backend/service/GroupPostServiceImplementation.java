473ccd69976826bb89f5f3fdb0b2ecd05c617ab5
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-16 09:22:24 +0200

    service-impl-parameter-commit

--- src/main/java/com/example/backend/Ihumure_backend/service/GroupPostServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/GroupPostServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.example.backend.Ihumure_backend.service;
 
+import com.example.backend.Ihumure_backend.model.GroupPost;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GroupPostServiceImplementation implements GroupPostService{
+    @Override
+    public List<GroupPost> getAllGroupPosts() {
+        return null;
+    }
+
+    @Override
+    public GroupPost getGroupPostById(Long id) {
+        return null;
+    }
+
+    @Override
+    public GroupPost save(GroupPost groupPost, Long groupId, Long authorId) {
+        return null;
+    }
+
+    @Override
+    public GroupPost update(Long id, GroupPost groupPost) {
+        return null;
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
 }
