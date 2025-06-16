6c9054613942644e891047ea2c6b9c144bae38f7
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-16 10:03:10 +0200

    service-impl-commit

--- src/main/java/com/example/backend/Ihumure_backend/service/NotificationServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/NotificationServiceImplementation.java
@@ -1,12 +1,24 @@
 package com.example.backend.Ihumure_backend.service;
 
 import com.example.backend.Ihumure_backend.model.Notification;
+import com.example.backend.Ihumure_backend.repository.NotificationRepository;
+import com.example.backend.Ihumure_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class NotificationServiceImplementation implements NotificationService{
+    private final NotificationRepository notificationRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public NotificationServiceImplementation(NotificationRepository notificationRepository, UserRepository userRepository) {
+        this.notificationRepository = notificationRepository;
+        this.userRepository = userRepository;
+    }
+
     @Override
     public List<Notification> getAllNotifications() {
         return null;
