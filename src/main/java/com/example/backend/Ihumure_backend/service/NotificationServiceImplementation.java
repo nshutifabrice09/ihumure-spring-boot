437ca9d8a629066c12ebb9d78744055dc150b948
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-16 09:54:44 +0200

    service-impl-paramters-commit

--- src/main/java/com/example/backend/Ihumure_backend/service/NotificationServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/NotificationServiceImplementation.java
@@ -1,7 +1,34 @@
 package com.example.backend.Ihumure_backend.service;
 
+import com.example.backend.Ihumure_backend.model.Notification;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NotificationServiceImplementation implements NotificationService{
+    @Override
+    public List<Notification> getAllNotifications() {
+        return null;
+    }
+
+    @Override
+    public Notification getNotificationById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Notification save(Notification notification, Long recipientId) {
+        return null;
+    }
+
+    @Override
+    public Notification update(Long id, Notification notification) {
+        return null;
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
 }
