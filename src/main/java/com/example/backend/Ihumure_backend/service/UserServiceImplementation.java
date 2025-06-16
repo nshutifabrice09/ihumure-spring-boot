da13b40bdb98bd79e86cba4ba2c371af961707bf
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-16 13:05:31 +0200

    service-impl-changes-commit

--- src/main/java/com/example/backend/Ihumure_backend/service/UserServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/UserServiceImplementation.java
@@ -1,12 +1,20 @@
 package com.example.backend.Ihumure_backend.service;
 
 import com.example.backend.Ihumure_backend.model.User;
+import com.example.backend.Ihumure_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserServiceImplementation implements UserService{
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImplementation (UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
     @Override
     public List<User> getAllUsers() {
         return null;
