c9f6f000936f522029436d5f85dea11dea905b8a
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-16 09:25:01 +0200

    repo-updates

--- src/main/java/com/example/backend/Ihumure_backend/service/GroupPostServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/GroupPostServiceImplementation.java
@@ -1,12 +1,28 @@
 package com.example.backend.Ihumure_backend.service;
 
 import com.example.backend.Ihumure_backend.model.GroupPost;
+import com.example.backend.Ihumure_backend.repository.GroupPostRepository;
+import com.example.backend.Ihumure_backend.repository.SupportGroupRepository;
+import com.example.backend.Ihumure_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class GroupPostServiceImplementation implements GroupPostService{
+    private final GroupPostRepository groupPostRepository;
+    private final SupportGroupRepository supportGroupRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public GroupPostServiceImplementation(GroupPostRepository groupPostRepository, SupportGroupRepository supportGroupRepository, UserRepository userRepository) {
+        this.groupPostRepository = groupPostRepository;
+        this.supportGroupRepository = supportGroupRepository;
+        this.userRepository = userRepository;
+    }
+
+
     @Override
     public List<GroupPost> getAllGroupPosts() {
         return null;
