4a8a780568d0f1490aea88bf0b8b1604fad0221c
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-16 09:53:46 +0200

    service-impl-methods-commit

--- src/main/java/com/example/backend/Ihumure_backend/service/NotificationServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/NotificationServiceImplementation.java
@@ -1,2 +1,7 @@
-package com.example.backend.Ihumure_backend.service;public class NotificationServiceImplementation {
+package com.example.backend.Ihumure_backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationServiceImplementation implements NotificationService{
 }
