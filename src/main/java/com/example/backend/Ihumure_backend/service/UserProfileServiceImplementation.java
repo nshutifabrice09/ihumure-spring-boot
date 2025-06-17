fc12c3a77b1b2824fd39f4af5f4e9e5c47a217b9
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-06-17 09:59:08 +0200

    initial-controller-commit

--- src/main/java/com/example/backend/Ihumure_backend/service/UserProfileServiceImplementation.java
+++ src/main/java/com/example/backend/Ihumure_backend/service/UserProfileServiceImplementation.java
@@ -1,5 +1,6 @@
 package com.example.backend.Ihumure_backend.service;
 
+import com.example.backend.Ihumure_backend.model.User;
 import com.example.backend.Ihumure_backend.model.UserProfile;
 import com.example.backend.Ihumure_backend.repository.UserProfileRepository;
 import com.example.backend.Ihumure_backend.repository.UserRepository;
@@ -31,16 +32,26 @@ public class UserProfileServiceImplementation implements UserProfileService{
 
     @Override
     public UserProfile saveUserProfile(UserProfile userProfile, Long userId) {
-        return null;
+        User user = userRepository.findUserById(userId);
+        userProfile.setUser(user);
+        return userProfileRepository.save(userProfile);
     }
 
     @Override
     public UserProfile updateUserProfile(Long id, UserProfile userProfile) {
+        UserProfile existUserProfile = userProfileRepository.findUserProfileById(id);
+        if(existUserProfile != null){
+            existUserProfile.setBio(userProfile.getBio());
+            existUserProfile.setLanguage(userProfile.getLanguage());
+            existUserProfile.setBirthDate(userProfile.getBirthDate());
+            existUserProfile.setGender(userProfile.getGender());
+            return userProfileRepository.save(existUserProfile);
+        }
         return null;
     }
 
     @Override
     public void removeUserProfile(Long id) {
-
+        userProfileRepository.deleteById(id);
     }
 }
