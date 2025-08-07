fbef7682ec9d681de83fa7c8cf25fbc38d906da2
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-07 10:01:24 +0200

    final-assessmentQuestionController-commit

--- src/main/java/com/example/backend/Ihumure_backend/controller/AppointmentController.java
+++ src/main/java/com/example/backend/Ihumure_backend/controller/AppointmentController.java
@@ -5,6 +5,8 @@ import com.example.backend.Ihumure_backend.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class AppointmentController {
@@ -20,6 +22,26 @@ public class AppointmentController {
     public Appointment saveAppointment(@RequestBody Appointment appointment,
                                        @PathVariable ("therapistId") Long therapistId, @PathVariable ("userId") Long userId){
         return appointmentService.saveAppointment(appointment, therapistId, userId);
+    }
+
+    @GetMapping("/appointments")
+    public List<Appointment> appointmentList(){
+        return appointmentService.getAllAppointments();
+    }
+
+    @GetMapping("/appointment/{id}")
+    public Appointment getAppointment(@PathVariable ("id") Long id){
+        return appointmentService.getAppointmentById(id);
+    }
+
+    @PutMapping("/update/appointment/{id}")
+    public Appointment updateAppointment(@PathVariable ("id") Long id, @RequestBody Appointment appointment){
+        return appointmentService.updateAppointment(id, appointment);
+    }
+
+    @DeleteMapping("/delete/appointment/{id}")
+    public void removeAppointment(@PathVariable ("id") Long id){
+        appointmentService.removeAppointment(id);
     }
 
 }
---
+++ src/main/java/com/example/backend/Ihumure_backend/controller/AssessmentAnswerController.java
@@ -0,0 +1,9 @@
+package com.example.backend.Ihumure_backend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class AssessmentAnswerRepository {
+}
