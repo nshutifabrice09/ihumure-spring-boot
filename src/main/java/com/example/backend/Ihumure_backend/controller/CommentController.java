234603ab6de50158571d81729c9f866a314043be
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-07 10:18:20 +0200

    final-commentController-commit

--- src/main/java/com/example/backend/Ihumure_backend/controller/CommentController.java
+++ src/main/java/com/example/backend/Ihumure_backend/controller/CommentController.java
@@ -1,2 +1,21 @@
-package com.example.backend.Ihumure_backend.controller;public class CommentController {
+package com.example.backend.Ihumure_backend.controller;
+
+
+import com.example.backend.Ihumure_backend.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    
 }
