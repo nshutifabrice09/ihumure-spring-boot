bb5010ee0538b3bc37fe472c2830e7ada8f0e4af
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-07 09:43:31 +0200

    final-appointmentController-commit

--- src/main/java/com/example/backend/Ihumure_backend/controller/AppointmentController.java
+++ src/main/java/com/example/backend/Ihumure_backend/controller/AppointmentController.java
@@ -1,2 +1,25 @@
-package com.example.backend.Ihumure_backend.controller;public class AppointmentController {
+package com.example.backend.Ihumure_backend.controller;
+
+import com.example.backend.Ihumure_backend.model.Appointment;
+import com.example.backend.Ihumure_backend.service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    @Autowired
+    public AppointmentController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+
+    @PostMapping("/appointment/{therapistId}/{userId}")
+    public Appointment saveAppointment(@RequestBody Appointment appointment,
+                                       @PathVariable ("therapistId") Long therapistId, @PathVariable ("userId") Long userId){
+        return appointmentService.saveAppointment(appointment, therapistId, userId);
+    }
+
 }
--- src/main/java/com/example/backend/Ihumure_backend/controller/TherapistController.java
+++ src/main/java/com/example/backend/Ihumure_backend/controller/TherapistController.java
@@ -1,2 +1,31 @@
-package com.example.backend.Ihumure_backend.controller;public class TherapistController {
+package com.example.backend.Ihumure_backend.controller;
+
+
+import com.example.backend.Ihumure_backend.model.Therapist;
+import com.example.backend.Ihumure_backend.service.TherapistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class TherapistController {
+
+    private final TherapistService therapistService;
+
+    @Autowired
+    public TherapistController(TherapistService therapistService) {
+        this.therapistService = therapistService;
+    }
+
+    @PostMapping("/therapist")
+    public Therapist saveTherapist(@RequestBody Therapist therapist){
+        return therapistService.saveTherapist(therapist);
+    }
+
+    @GetMapping("therapists")
+    public List<Therapist> therapistList(){
+        return therapistService.getAlTherapists();
+    }
 }
