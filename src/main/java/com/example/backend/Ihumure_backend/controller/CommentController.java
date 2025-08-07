d4a7d7f887630b44e10c373f51abbb719fff6857
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-07 10:38:55 +0200

    final-groupPostController-commit

--- src/main/java/com/example/backend/Ihumure_backend/controller/CommentController.java
+++ src/main/java/com/example/backend/Ihumure_backend/controller/CommentController.java
@@ -1,10 +1,12 @@
 package com.example.backend.Ihumure_backend.controller;
 
 
+import com.example.backend.Ihumure_backend.model.Comment;
 import com.example.backend.Ihumure_backend.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -17,5 +19,28 @@ public class CommentController {
         this.commentService = commentService;
     }
 
-    
+    @PostMapping("/comment/{postId}/{authorId}")
+    public Comment saveComment(@RequestBody Comment comment, @PathVariable("postId") Long postId, @PathVariable ("authorId") Long authorId){
+        return commentService.saveComment(comment, postId, authorId);
+    }
+
+    @GetMapping("/comments")
+    public List<Comment> commentList(){
+        return commentService.getAllComments();
+    }
+
+    @GetMapping("/comment/{id}")
+    public Comment getComment(@PathVariable("id") Long id){
+        return commentService.getCommentById(id);
+    }
+
+    @PutMapping("/update/comment/{id}")
+    public Comment updateComment(@PathVariable ("id") Long id, @RequestBody Comment comment){
+        return commentService.updateComment(id, comment);
+    }
+
+    @DeleteMapping("/Delete/comment/{id}")
+    public void deleteComment(@PathVariable ("id") Long id){
+        commentService.removeComment(id);
+    }
 }
