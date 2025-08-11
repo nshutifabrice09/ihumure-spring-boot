8feb77db3b9036fd5519a76f983235a354d514fa
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-11 12:35:40 +0200

    initial-NotificationController-commit

--- src/main/java/com/example/backend/Ihumure_backend/controller/AssessmentController.java
+++ src/main/java/com/example/backend/Ihumure_backend/controller/AssessmentController.java
@@ -1,18 +1,46 @@
 package com.example.backend.Ihumure_backend.controller;
 
-import com.example.backend.Ihumure_backend.repository.AssessmentRepository;
+import com.example.backend.Ihumure_backend.model.Assessment;
+import com.example.backend.Ihumure_backend.service.AssessmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class AssessmentController {
 
-    private final AssessmentRepository assessmentRepository;
+    private final AssessmentService assessmentService;
 
     @Autowired
-    public AssessmentController(AssessmentRepository assessmentRepository) {
-        this.assessmentRepository = assessmentRepository;
+    public AssessmentController(AssessmentService assessmentService) {
+        this.assessmentService = assessmentService;
+    }
+
+
+    @PostMapping("/assessment/{userId}")
+    public Assessment saveAssessment(@RequestBody Assessment assessment, @PathVariable("userId") Long userId){
+        return assessmentService.saveAssessment(assessment, userId);
+    }
+
+    @GetMapping("/assessments")
+    public List<Assessment> assessmentList(){
+        return assessmentService.getAllAssessments();
+    }
+
+    @GetMapping("/assessment/{id}")
+    public Assessment getAssessment(@PathVariable ("id") Long id){
+        return assessmentService.getAssessmentById(id);
+    }
+
+    @PutMapping("/update/assessment/{id}")
+    public Assessment updateAssessment (@PathVariable ("id") Long id, @RequestBody Assessment assessment){
+        return assessmentService.updateAssessment(id, assessment);
+    }
+
+    @DeleteMapping("/delete/assessment/{id}")
+    public void deleteAssessment(@PathVariable ("id") Long id){
+        assessmentService.removeAssessment(id);
     }
 }
