a7a65d70bda9e5983dae636ca04dbb0c25f8fe2e
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-11 12:06:39 +0200

    final-assessmentController-commit

--- src/main/java/com/example/backend/Ihumure_backend/controller/AssessmentController.java
+++ src/main/java/com/example/backend/Ihumure_backend/controller/AssessmentController.java
@@ -1,2 +1,18 @@
-package com.example.backend.Ihumure_backend.controller;public class AssessmentController {
+package com.example.backend.Ihumure_backend.controller;
+
+import com.example.backend.Ihumure_backend.repository.AssessmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class AssessmentController {
+
+    private final AssessmentRepository assessmentRepository;
+
+    @Autowired
+    public AssessmentController(AssessmentRepository assessmentRepository) {
+        this.assessmentRepository = assessmentRepository;
+    }
 }
