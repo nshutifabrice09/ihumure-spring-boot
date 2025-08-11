78a0d1add1ab5398594965a053e86d60bb063133
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-11 12:40:11 +0200

    final-NotificationController-commit

--- src/main/java/com/example/backend/Ihumure_backend/controller/NotificationController.java
+++ src/main/java/com/example/backend/Ihumure_backend/controller/NotificationController.java
@@ -1,9 +1,11 @@
 package com.example.backend.Ihumure_backend.controller;
 
+import com.example.backend.Ihumure_backend.model.Notification;
 import com.example.backend.Ihumure_backend.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("/http://localhost/3000")
@@ -15,4 +17,15 @@ public class NotificationController {
     public NotificationController(NotificationService notificationService) {
         this.notificationService = notificationService;
     }
+
+    @PostMapping("/notification/{recipientId}")
+    public Notification save(@RequestBody Notification notification, @PathVariable ("recipientId") Long recipientId){
+        return notificationService.saveNotification(notification, recipientId);
+    }
+
+    @GetMapping("/notifications")
+    public List<Notification> notificationList(){
+        return notificationService.getAllNotifications();
+    }
+    
 }
