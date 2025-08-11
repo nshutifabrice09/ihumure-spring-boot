064551851989b52ef6d3235779783115a0207e19
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-11 11:16:27 +0200

    initial-assessmentController-commit

--- src/main/java/com/example/backend/Ihumure_backend/controller/SupportGroupController.java
+++ src/main/java/com/example/backend/Ihumure_backend/controller/SupportGroupController.java
@@ -1,2 +1,45 @@
-package com.example.backend.Ihumure_backend.controller;public class SupportGroupController {
+package com.example.backend.Ihumure_backend.controller;
+
+import com.example.backend.Ihumure_backend.model.SupportGroup;
+import com.example.backend.Ihumure_backend.service.SupportGroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class SupportGroupController {
+
+    private final SupportGroupService supportGroupService;
+
+    @Autowired
+    public SupportGroupController(SupportGroupService supportGroupService) {
+        this.supportGroupService = supportGroupService;
+    }
+
+    @PostMapping("/supportGroup")
+    public SupportGroup save(@RequestBody SupportGroup supportGroup){
+        return supportGroupService.saveSupportGroup(supportGroup);
+    }
+
+    @GetMapping("/supportGroups")
+    public List<SupportGroup> supportGroupList(){
+        return supportGroupService.getAllSupportGroups();
+    }
+
+    @GetMapping("/supportGroup/{id}")
+    public SupportGroup getSupportGroup(@PathVariable ("id") Long id){
+        return supportGroupService.getSupportGroupById(id);
+    }
+
+    @PutMapping("/update/supportGroup/{id}")
+    public SupportGroup updateSupportGroup(@PathVariable ("id") Long id, @RequestBody SupportGroup supportGroup){
+        return supportGroupService.updateSupportGroup(id, supportGroup);
+    }
+
+    @DeleteMapping("/delete/supportGroup/{id}")
+    public void deleteSupportGroup(@PathVariable ("id") Long id){
+        supportGroupService.removeSupportGroup(id);
+    }
 }
